69ee3d43da8fe758108301258a655e1464d3a5a4
Thomas Kruchelski <thomas.lima.kru@gmail.com>
2025-06-12 15:10:00 -0300

    waves bg

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -4,6 +4,9 @@ import { baseOptions } from '@/app/layout.config';
 import { source } from '@/lib/source';
 
 export default function Layout({ children }: { children: ReactNode }) {
+
+  
+
   return (
     <DocsLayout tree={source.pageTree} {...baseOptions}>
       {children}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,35 @@ import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
+import Waves from '@/components/Waves'
 
 const inter = Inter({
   subsets: ['latin'],
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
+
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+      <body className="flex flex-col min-h-screen relative">
+        <div className='w-full h-full z-10 fixed'>
+          <Waves
+            lineColor="#130240"
+            backgroundColor="#060010"
+            waveSpeedX={0.02}
+            waveSpeedY={0.01}
+            waveAmpX={40}
+            waveAmpY={20}
+            friction={0.9}
+            tension={0.01}
+            maxCursorMove={15}
+            xGap={12}
+            yGap={36}
+          />
+        </div>
+        <div className='w-full h-full z-20'>
+          <RootProvider>{children}</RootProvider>
+        </div>
       </body>
     </html>
   );
