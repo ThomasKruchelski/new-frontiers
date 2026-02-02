b0361a29ce997ce116b4862732be96865026c162
Thomas Kruchelski <thomas.lima.kru@gmail.com>
2026-02-02 13:45:47 -0300

    dezember 2025 att

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import BackGroundVideo from '@/components/BackgroundVideo'
 
 import Post1 from 'src/public/posts/post-0-1.gif'
-import Post2 from 'src/public/posts/post-0-5.gif'
+import Post2 from 'src/public/posts/post-0-8.gif'
 
 export default function HomePage() {
   // console.log(gifbg)
@@ -29,6 +29,37 @@ export default function HomePage() {
 
         </div>
       </div> */}
+
+      <div className='flex flex-col items-center w-full pb-10'>
+
+        <h1 className="mb-4 text-4xl font-bold text-center">Update 0.8 - Preparativos para Versão Piloto</h1>
+        <Image src={Post2} alt='' className=''></Image>
+        <div className='w-full px-4 md:px-0 md:max-w-11/20 pt-4 flex flex-col gap-3'>
+          <p className='w-full'>FALA GALERA, quase 3 aninhos sem post né...</p>
+          <p className='w-full'>
+            Apesar da ausência, continuei trabalhando arduamente no sistema, muitas coisas mudaram e estamos próximos de lançar uma versão piloto.
+          </p>
+          <p>
+            O principal foco que vim trabalhando nesses últimos tempos foi a ficha dos personagens e todo o sistema de regras direcionado a eles, hoje já temos uma ficha funcional com mecanicas direcionadas ao uso pericias utilizando o sistema d100. O sistema conta com 7 tipos de cenas diferentes para contar histórias cyberpunk imersivas focadas na narração e na criatividade dos jogadores, sem deixar de lado o aspecto de combate inclusivo para todos os tipos de personagem e jogadores.
+          </p>
+          
+          <i>"Tá, mas e os robôs gigantes?"</i>
+
+          <p>Trabalhei bastante no aspecto criativo das "funções" que os robos poderiam executar em combate, então nasceu um sistema de combate baseado em talentos dando a liberdade do jogador montar o que ele deseja do seu robo, se ele deseja dano, basta pegar um talento de aumento de ataque, se ele quer atacar mais de uma vez, basta pegar o talento.</p>
+          <p>Hoje o sistema funciona de forma bem simplista e se forçado um pouco é facil encontrar combos desbalanceados, então ainda havera um rework geral de como quero que o combate funcione em atualizações futuras.</p>
+
+          <i>"No que está trabalahndo agora?"</i>
+
+          <p>Meu objetivo é concluir o sistema de personagens, criar e balancear os equipamentos para tornar solida e base do sistema. Depois vou dar foco nos robos gigantes novamente e lança-lo como um conteúdo adicional, assim, para quem mestrar uma campanha e deseja só usar o sistema de personagem, basta não usar o conteúdo adicional dos robos gigantes.</p>
+
+          <p>Esse foi um post mais longo, mas sinto que era importânte deixar todos informados sobre o caminho que o sistema está indo, quem tem interesse em mais notícias ou tenha ideas pode entrar em contato comigo através do nosso discord oficial <a className='underline bold text-sky-600' href='https://discord.gg/CpSPQNnH5T' target='blank_'>clicando aqui</a>. Vejo vocês em breve ;)</p>
+          
+          <p className='w-full text-end'>
+            - Thom  29/01/2026
+          </p>
+
+        </div>
+      </div>
 
       <div className='flex flex-col items-center w-full pb-10'>
 
