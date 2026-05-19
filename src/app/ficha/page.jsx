466dcade8f1aec4a28bc8ec7f7a38235bab285b3
Thomas Kruchelski <thomas.lima.kru@gmail.com>
2026-05-19 12:59:48 -0300

    update `ficha` pagewith placeholder

--- src/app/ficha/page.jsx
+++ src/app/ficha/page.jsx
@@ -42,9 +42,29 @@ const DEFAULT_SHEET = {
 export default function CharacterSheet() {
 
   return (
-    <div className="max-h-screen bg-background relative flex flex-1  justify-center">
-      <h1 className="pt-10">Em construção...</h1>
-    </div>
+    <main className="flex flex-1 flex-col items-center pb-10 w-full">
+
+      <div className=" max-w-[500px] box-content border-b border-fd-foreground/10 transition-colors p-4 px-8 lg:mt-4 lg:w-[calc(100%-1rem)] lg:rounded-2xl lg:border shadow-sm bg-fd-background/80 backdrop-blur-lg">
+        <b> Ficha Excel</b>
+
+        <p>1. Acesse o arquivo no Google docs 
+          <a className="hyperlink pl-2" target='_blank' href="https://docs.google.com/spreadsheets/d/1cZezQt8fyfF2wT1SN6AYVOKbV6HbRo4kANuCDSkdP7c/edit?gid=1362246236#gid=1362246236">
+            Clicando aqui
+          </a>
+        </p>
+
+        <p>2. Acesse o menu Arquivo: Fazer uma cópia</p>
+
+        <p>3. Acesse e preencha a cópia criada</p>
+      </div>
+
+      <div className=" max-w-[500px] box-content border-b border-fd-foreground/10 transition-colors p-4 px-8 lg:mt-4 lg:w-[calc(100%-1rem)] lg:rounded-2xl lg:border shadow-sm bg-fd-background/80 backdrop-blur-lg">
+        <b> Ficha no Site</b>
+
+        <p>Em construção...</p>
+        
+      </div>
+    </main>
     //     <SheetToolbar
     //       characters={characters}
     //       activeId={activeId}
